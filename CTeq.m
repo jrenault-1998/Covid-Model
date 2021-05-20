43afd82a74e882591d1d53ab685151c67669a490
jrenault-1998 <joshjrenault@gmail.com>
2021-05-19 17:23:27 -0700

    DDE Functions inserted
    I did a total workup on the equations to match the dde function. The code doesn't run yet

--- CTeq.m
+++ CTeq.m
@@ -1,40 +1,55 @@
-function dy = CTeq(t,y)
+function dydt = CTeq(t,y, Z)
 
-global alpha C bc ba h tau deltaE deltaIp deltaIc deltaIa deltaQ deltaQa r epsilon Cv x1 x2 x3 x4 v deltasq deltaIv N
+global alpha C bc ba q deltaE deltaIp deltaIc deltaIa deltaQ r epsilon1 epsilon2 Cv  v deltaSq deltaSv1 deltaIv N vmax vstop
 
+ylag1 = Z(:,1);     %For Z(:,i), these values are t-i days before
+ylag2 = Z(:,2);
+ylag3 = Z(:,3);
+ylag4 = Z(:,4);
+ylag5 = Z(:,5);
+ylag6 = Z(:,6);
 
-S  = y(1);  
+S  = y(1);
+S1 = ylag1(1);
+S2 = ylag2(1);      %For "ylagi(j)", this is y(j) at t-i days before
+S3 = ylag3(1);
+S4 = ylag4(1);
+S5 = ylag5(1);
 E  = y(2);
+E6 = ylag6(2);
 Ip = y(3);
 Ic = y(4);
 Ia = y(5);
 Q  = y(6);
-Qa = y(7);
-Sq = y(8);
-Sv = y(9);
-Iv = y(10);
+Sq = y(7);
+Sv1 = y(8);
+Iv1 = y(9);
+Iv2 = y(10);
 R  = y(11);
-Rv = y(12);
+Sv2 = y(12);
 
-dy = zeros(12,1);
 
-if Sv + Iv + Rv > 0.89
+
+dy = zeros(11,1);
+
+if Sv1 + Iv1 + Iv2 + Sv2 > vmax
     v=0;
-else
-    v=0.06/7;
-
-
-dy(1)  = -S*alpha*C*(Ip+bc*Ic+ba*Ia)/N - Ic*h*(1-alpha)*C*tau*S/N + deltasq*Sq - v*N*S/(S+R);
-dy(2)  = S*alpha*C*(Ip+bc*Ic+ba*Ia)/N - Ic*h*alpha*C*(1/deltaE)*S/N - deltaE*E;
-dy(3)  = r*deltaE*E - Ic*h*alpha*C*x1*S/N - deltaIp*Ip;
-dy(4)  = deltaIp*Ip - deltaIc*Ic;
-dy(5)  = (1-r)*deltaE*E - Ic*h*alpha*C*x2*S/N - deltaIa*Ia;
-dy(6)  = Ic*h*alpha*C*x3*S/N - deltaQ*Q;
-dy(7)  = Ic*h*alpha*C*x4*S/N - deltaQa*Qa;
-dy(8)  = Ic*h*(1-alpha)*C*tau*S/N - deltasq*Sq; 
-dy(9)  = v*N*S/(S+R)-Sv*alpha*Cv*(1-epsilon)*(Ip+bc*Ic+ba*Ia)/N;
-dy(10) = Sv*alpha*Cv*(1-epsilon)*(Ip+bc*Ic+ba*Ia)/N - deltaIv*Iv;
-dy(11) = deltaIc*Ic+deltaIa*Ia+deltaQ*Q+ deltaQa*Qa-v*N*R/(S+R);
-dy(12) = deltaIv*Iv + v*N*R/(S+R);
-
 end
+
+
+if Sv1 + Iv1 + Iv2 + Sv2 > vstop
+    q=0;
+end
+
+dy(1)  = -S*alpha*C*(Ip+bc*Ic+ba*Ia)/N - (E6*r*deltaE)*q*(1-alpha)*C*(bc*(S+S1)+S2+S3+S4+S5) - v*N*S/(S+R) + deltaSq*Sq;
+dy(2)  =  S*alpha*C*(Ip+bc*Ic+ba*Ia)/N - (E6*r*deltaE)*q*alpha*C*(bc*(S+S1)+S2+S3) - deltaE*E;
+dy(3)  = r*deltaE*E - (E6*r*deltaE)*r*q*alpha*C*(S4+S5) - deltaIp*Ip;
+dy(4)  = deltaIp*Ip - deltaIc*Ic;
+dy(5)  = (1-r)*deltaE*E - (E6*r*deltaE)*(1-r)*q*alpha*C*(S4+S5) - deltaIa*Ia;
+dy(6)  = (E6*r*deltaE)*q*alpha*C*(bc*(S+S1)+S2+S3+S4+S5) - deltaQ*Q;
+dy(7)  = (E6*r*deltaE)*q*(1-alpha)*C*(bc*(S+S1)+S2+S3+S4+S5) - deltaSq*Sq; 
+dy(8)  = v*N*S/(S+R)-Sv1*alpha*Cv*(1-epsilon1)*(Ip+bc*Ic+ba*Ia)/N - deltaSv1*Sv1;
+dy(9)  = Sv1*alpha*Cv*(1-epsilon1)*(Ip+bc*Ic+ba*Ia)/N - deltaIv*Iv1;
+dy(10) = Sv2*alpha*Cv*(1-epsilon2)*(Ip+bc*Ic+ba*Ia)/N - deltaIv*Iv1;
+dy(11) = deltaIc*Ic + deltaIa*Ia + deltaQ*Q - v*N*R/(S+R);
+dy(12) = v*N*R/(S+R) + deltaIv*Iv1 + deltaSv1*Sv1 + deltaIv*Iv2 - Sv2*alpha*Cv*(1-epsilon2)*(Ip+bc*Ic+ba*Ia)/N;
--- CTmodel.m
+++ CTmodel.m
@@ -1,98 +1,112 @@
 close all
 clear all
+clc
+
 
 
 %model parameters
-global alpha C bc ba h tau deltaE deltaIc deltaIa r epsilon Cv deltasq deltaIv deltaQ deltaQa x1 x2 x3 x4 N deltaIp v
+global alpha C bc ba q deltaE deltaIc deltaIa r epsilon1 epsilon2 Cv deltaSq deltaSv1 deltaIv deltaQ N deltaIp v vmax vstop
 
 alpha = 0.18;
-C = 0.75/alpha;     %beta = alpha*C s.t. beta=0.75
-bc = 0.5;           %reduction in contacts|symptomatic?
+C = 0.5/alpha;             %beta = alpha*C s.t. beta=0.75
+bc = 0.5;                   %reduction in contacts|symptomatic?
 ba = 0.75;
-h =0.8;             %Contact tracing efficacy
-tau = 4;            %Estimate, I need to find this ave. value
+q =0.8;                     %Contact tracing efficacy
+tau = 2;                    %Estimates time from entering Ic to CTing
 deltaE = 1/4;
 deltaIp = 1/2.4;
-deltaIc =1/3.2;
+deltaIc =1/3.2;             
 deltaIa = 1/7;
-deltasq = 1/10;     %How long are people told to isolate for?
-deltaIv = 1/5;      %Average time spent infectious|vaccinated?
+deltaSq = 1/10;              %How long are people told to isolate for?
+deltaIv = 1/5;               %Average time spent infectious|vaccinated?
 deltaQ = 1/10;
-deltaQa = 1/10;
+deltaSv1 = 1/100;            %Days between first and second dose?
 r = 0.7;
-epsilon = 0.9;
-Cv = C + 0.5;       %increase by some constant?
-x1 = min(1/deltaIp, tau-1/deltaE);
-x2 = min(1/deltaIa, tau);
-x3 = x1+r/deltaE;
-x4 = x2+(1-r)/deltaE;
-v = 0.06/7;         %0.06 of pop. every week
+epsilon1 = 0.7;
+epsilon2 = 0.9;
+Cv = C + 0.5;                %increase by some constant?
+v = 0.06/7;                  %0.06 of pop. every week
 N = 1;
-totalpop = 5.2e5;   %Population of Newfoundland
+totalpop = 5.2e5;            %Population of Newfoundland
+vmax = 462000/totalpop;      %# of people eligible for vaccine
+vstop = 0.5;                 %stop CTing when vstop people are vaccinated
 
 
-s0=0.9-3/(totalpop);
-e0=0.1;
-ip=1/(totalpop); %Infectious, Presymptomatic
-ic=1/(totalpop);
-ia=1/(totalpop);
-q=0;
-qa=0;
-sq=0;
-sv=0;
-iv=0;
-re=0;
-rv=0;
+s0=1-3/(totalpop);
+e0=0;
+ip0=1/(totalpop); %Infectious, Presymptomatic
+ic0=1/(totalpop);
+ia0=1/(totalpop);
+q0=0;
+sq0=0;
+sv0=0;
+iv0=0;
+re0=0;
+rv0=0;
 
 Tf = 120; %days of simulation
 
-options = odeset('RelTol',1e-4,'AbsTol',1e-6);
-%[T,Y] = ode45(@CTeq, 0:1:Tf, [s0;e0;ip;ic;ia;q;qa;sq;sv;iv;re;rv], options);
+sol = dde23(@CTeq,[1, 2, 3, 4, 5, 6],@ddex1hist,[0, Tf]);
 
-options1 = odeset(options,'NonNegative', 1:12);   %Need NonNegative results
-[T,Y] = ode45(@CTeq, 0:1:Tf, [s0;e0;ip;ic;ia;q;qa;sq;sv;iv;re;rv], options1);
+% options = odeset('RelTol',1e-4,'AbsTol',1e-6);
+% [T,Y] = ode45(@CTeq, 0:1:Tf, [s0;e0;ip0;ic0;ia0;q0;sq0;sv0;iv0;re0;rv0], options);
 
-pop = sum(Y');
+% options1 = odeset(options,'NonNegative', 1:12);   %Need NonNegative results
+% [T,Y] = ode45(@CTeq, 0:1:Tf, [s0;e0;ip0;ic0;ia0;q0;sq0;sv0;iv0;re0;rv0], options1);
+% 
+% options = odeset('RelTol',1e-4,'AbsTol',1e-6);
+% Y(1, :)= [s0;e0;ip0;ic0;ia0;q0;sq0;sv0;iv0;re0;rv0];
+% T(1) = 0;
 
-figure(1)
+% for i = 1:Tf
+% %     [TempT,TempY] = ode45(@CTeq, i-1:i, Y(i, :), options);
+% %     T(i+1) = TempT(end);
+% %     Y(i+1,:) = TempY(end,:);
+%     Tempdy = CTeq(i, Y(i, :));
+%     
+%     Y(i+1, :) = Tempdy' + Y(i, :);
+% end
+
+% 
+% figure(1)
+% 
+% 
+% subplot(1,2,1)
+% title('...')
+% plot(T,Y(:,10),'y','Linewidth',1)
+% hold on
+% plot(T,Y(:,2)+Y(:,3)+Y(:,4)+Y(:,5),'k','Linewidth',1)
+% hold on
+% plot(T,Y(:,8)+Y(:,9)+Y(:,11),'r','Linewidth',1)
+% hold on
+% plot(T,Y(:,6)+Y(:,7),'m','Linewidth',1)
+% hold on
+% plot(T,Y(:,8),'c','Linewidth',1)
+% hold on
+% plot(T,Y(:,11),'g','Linewidth',1)
+% legend('I','location','best')
+% ylabel('')
+% xlabel('time [days]')
+% %axis([ 0 Tf -0.1 55])
+% set(gca,'fontsize',14)
 
 
-subplot(1,2,1)
-title('...')
-plot(T,Y(:,1),'y','Linewidth',1)
-hold on
-plot(T,Y(:,2),'k','Linewidth',1)
-hold on
-plot(T,Y(:,3),'r','Linewidth',1)
-hold on
-plot(T,Y(:,4),'m','Linewidth',1)
-hold on
-plot(T,Y(:,5),'c','Linewidth',1)
-hold on
-plot(T,Y(:,6),'g','Linewidth',1)
-%legend('I','location','best')
-%ylabel('')
-xlabel('time [days]')
-%axis([ 0 Tf -0.1 55])
-set(gca,'fontsize',14)
-
-
-subplot(1,2,2)
-plot(T,Y(:,7),'r','Linewidth',1)
-hold on
-plot(T,Y(:,8),'c','Linewidth',1)
-hold on
-plot(T,Y(:,9),'g','Linewidth',1)
-hold on
-plot(T,Y(:,10),'b','Linewidth',1)
-hold on
-plot(T,Y(:,11),'y','Linewidth',1)
-%legend('I','location','best')
-%ylabel('')
-xlabel('time [days]')
-%axis([ 0 Tf -0.1 55])
-set(gca,'fontsize',14)
-
+% subplot(1,2,2)
+% plot(T,Y(:,7),'r','Linewidth',1)
+% hold on
+% plot(T,Y(:,8),'c','Linewidth',1)
+% hold on
+% plot(T,Y(:,9),'g','Linewidth',1)
+% hold on
+% plot(T,Y(:,10),'b','Linewidth',1)
+% hold on
+% plot(T,Y(:,11),'y','Linewidth',1)
+% %legend('I','location','best')
+% %ylabel('')
+% xlabel('time [days]')
+% %axis([ 0 Tf -0.1 55])
+% set(gca,'fontsize',14)
+% 
 
 
 
